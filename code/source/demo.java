b471eb0cc4686ea753f77bbc7f231cd0e2b768f5
ptiw <ptiw@softwareag.com>
2024-08-02 10:54:04 +0530

    abc

--- code/source/demo.java
+++ code/source/demo.java
@@ -7,6 +7,8 @@ import com.wm.util.Values;
 import com.wm.app.b2b.server.Service;
 import com.wm.app.b2b.server.ServiceException;
 // --- <<IS-START-IMPORTS>> ---
+import com.wm.lang.ns.NSName;
+import com.softwareag.util.IDataMap;
 // --- <<IS-END-IMPORTS>> ---
 
 public final class demo
@@ -25,18 +27,36 @@ public final class demo
 
 
 
+	public static final void addNumJS (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(addNumJS)>> ---
+		// @sigtype java 3.5
+		// [i] field:0:required a
+		// [i] field:0:required b
+		// [o] field:0:required c
+		IDataMap input = new IDataMap();
+		input.put("num1", new IDataMap(pipeline).getAsString("a"));
+		input.put("num2", new IDataMap(pipeline).getAsString("b"));
+		try {
+			IData doInvoke = Service.doInvoke(NSName.create("pub.math:addInts"), input.getIData());
+			new IDataMap(pipeline).put("c", new IDataMap(doInvoke).getAsString("value"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// --- <<IS-END>> ---
+
+                
+	}
+
+
+
 	public static final void new_javaService (IData pipeline)
         throws ServiceException
 	{
 		// --- <<IS-START(new_javaService)>> ---
 		// @sigtype java 3.5
-		try {
-			Thread.sleep(300000);
-			
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		// --- <<IS-END>> ---
 
                 
--- ns/demo/new_javaService/java.frag
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-
-<Values version="2.0">
-  <value name="name">new_javaService</value>
-  <value name="encodeutf8">true</value>
-  <value name="body">dHJ5IHsNCglUaHJlYWQuc2xlZXAoMzAwMDAwKTsNCgkNCn0gY2F0Y2ggKEludGVycnVwdGVkRXhj
-ZXB0aW9uIGUpIHsNCgkvLyBUT0RPIEF1dG8tZ2VuZXJhdGVkIGNhdGNoIGJsb2NrDQoJZS5wcmlu
-dFN0YWNrVHJhY2UoKTsNCn0=</value>
-</Values>
